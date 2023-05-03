8c5af9df55d814833b38dd42e7503ff5b5e760f7
kkoliveiraa <RM550815@fiap.com.br>
2023-05-03 20:52:15 -0300

    mexendo com rgb no js, interpolação e tals

--- js/script.js
+++ js/script.js
@@ -8,17 +8,39 @@
 // //função parseInt...;
 // console.log(parseInt(nr1)+parseInt(nr2));
 
-var nome1 = "Dubas";
-// let nome2 = "Serbatinho";
-// const nome3 = "Julia";
+// var nome1 = "Dubas";
+// // let nome2 = "Serbatinho";
+// // const nome3 = "Julia";
 
-if(nome1 != ""){
-    let nome1 = "Julia";
-}
+// if(nome1 != ""){
+//     let nome1 = "Julia";
+// }
+
+// var nome
+
+// console.log(nome1);
 
 
 
-var nome
+/* <button value="click" id="meu-btn" >Click</button> */
+const btn = document.getElementById("meu-btn");
+//                   EVENTO     FUNÇÃO CALLBACK
+//PRIMEIRO PASSO PARA A TRANSFORMAÇÃO DO BOTÃO EM RGB...
+btn.addEventListener("click", function(){
+    //console.log(this.textContent);
+    //Função matemática Math
+    //random = Retorna um número aleatório entre 0 e 1 : Ex: 0.2323454545;
+    //floor = Arredonda o número para baixo.
+    //ceil = Arredonda o número para cima.
+    //round = Arredonda o número aleatório.
+    let r=0,g=0,b=0;
+
+    r= Math.round(Math.random() * 255);
+    g= Math.round(Math.random() * 255);
+    b= Math.round(Math.random() * 255);
+    
+    //this.setAttribute("style", "background-color:rgb("+r+","+g+","+b+");");
+    this.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
 
 
-console.log(nome1);
+});
