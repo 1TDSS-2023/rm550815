3f65b35e97a454c9cfcc00f5c8117cc19bd14f6f
kkoliveiraa <RM550815@fiap.com.br>
2023-05-05 19:48:59 -0300

    Declaração de array e seus métodos, até splice.

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@
 const btn = document.getElementById("meu-btn");
 //                   EVENTO     FUNÇÃO CALLBACK
 //PRIMEIRO PASSO PARA A TRANSFORMAÇÃO DO BOTÃO EM RGB...
-btn.addEventListener("click", function(){
+// btn.addEventListener("click", function(){
     //console.log(this.textContent);
     //Função matemática Math
     //random = Retorna um número aleatório entre 0 e 1 : Ex: 0.2323454545;
@@ -35,20 +35,56 @@ btn.addEventListener("click", function(){
     //round = Arredonda o número aleatório.
     let r=0,g=0,b=0;
 
-    r= Math.round(Math.random() * 255);
-    g= Math.round(Math.random() * 255);
-    b= Math.round(Math.random() * 255);
+    // r= Math.round(Math.random() * 255);
+    // g= Math.round(Math.random() * 255);
+    // b= Math.round(Math.random() * 255);
     
-    //this.setAttribute("style", "background-color:rgb("+r+","+g+","+b+");");
-    this.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
+    // //this.setAttribute("style", "background-color:rgb("+r+","+g+","+b+");");
+    // this.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
 
 
-});
+// });
 
 /* <h2 class="tit-sec">Seção de Fotos</h2> */
 
-const h2Element = document.querySelector(".tit-sec");
+// const h2Element = document.querySelector(".tit-sec");
 
-console.log(h2Element);
+// console.log(h2Element);
 
 //querySelectorAll()
+
+//Declaração de um ARRAY e seus métodos!
+let frutas = ["banana","maçã","laranja","morango","uva","amora","cereja","abacaxi"];
+//Imprimindo um array
+console.log(frutas)
+console.log(frutas[7])
+//Verificando o tamanho do array através da propriedade length
+console.log(`O tamanho do Array é : ${frutas.length}`);
+
+//Inserindo um item ao final do array com o método push(nomeItem);
+frutas.push("pêssego");
+console.log(frutas)
+//Inserindo um item ao final do array com o método unshift(nomeItem);
+frutas.unshift("kiwi");
+console.log(frutas)
+
+//Removendo um item ao final do array com o método pop();
+frutas.pop();
+console.log(frutas)
+//Removendo um item no inicio do array com o método shift();
+frutas.shift()
+console.log(frutas)
+
+//Localizando um item no array com o método indexOf(nomeItem);
+//Obs: o método indexOf(nomeItem) retorna o indice do item.
+// let indice = frutas.indexOf("uva");
+// console.log(`Indice do item buscado ${indice}`);
+// console.log(`Elemento buscado ${frutas[indice]}`);
+
+//Removendo um item do array com o método splice(indice do item, qtd de vezes que o indice será removido !)
+//Obs: Utilize o método indexOf(nomeItem) para descobrir o indice do item.
+let indice = frutas.indexOf("amora");
+console.log(`Elemento no indice antes da remoção: ${frutas[indice]}`);
+frutas.splice(indice, 1);
+console.log(frutas);
+console.log(`Elemento no indice após a remoção: ${frutas[indice]}`);
