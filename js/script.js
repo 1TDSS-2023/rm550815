d8b567aaba44b4db44216b224b2e9b45bfed9774
kkoliveiraa <RM550815@fiap.com.br>
2023-05-05 20:14:54 -0300

    concatenando array com o tal do spread

--- js/script.js
+++ js/script.js
@@ -54,26 +54,26 @@ const btn = document.getElementById("meu-btn");
 //querySelectorAll()
 
 //Declaração de um ARRAY e seus métodos!
-let frutas = ["banana","maçã","laranja","morango","uva","amora","cereja","abacaxi"];
-//Imprimindo um array
-console.log(frutas)
-console.log(frutas[7])
-//Verificando o tamanho do array através da propriedade length
-console.log(`O tamanho do Array é : ${frutas.length}`);
+// let frutas = ["banana","maçã","laranja","morango","uva","amora","cereja","abacaxi"];
+// //Imprimindo um array
+// console.log(frutas)
+// console.log(frutas[7])
+// //Verificando o tamanho do array através da propriedade length
+// console.log(`O tamanho do Array é : ${frutas.length}`);
 
-//Inserindo um item ao final do array com o método push(nomeItem);
-frutas.push("pêssego");
-console.log(frutas)
-//Inserindo um item ao final do array com o método unshift(nomeItem);
-frutas.unshift("kiwi");
-console.log(frutas)
+// //Inserindo um item ao final do array com o método push(nomeItem);
+// frutas.push("pêssego");
+// console.log(frutas)
+// //Inserindo um item ao final do array com o método unshift(nomeItem);
+// frutas.unshift("kiwi");
+// console.log(frutas)
 
-//Removendo um item ao final do array com o método pop();
-frutas.pop();
-console.log(frutas)
-//Removendo um item no inicio do array com o método shift();
-frutas.shift()
-console.log(frutas)
+// //Removendo um item ao final do array com o método pop();
+// frutas.pop();
+// console.log(frutas)
+// //Removendo um item no inicio do array com o método shift();
+// frutas.shift()
+// console.log(frutas)
 
 //Localizando um item no array com o método indexOf(nomeItem);
 //Obs: o método indexOf(nomeItem) retorna o indice do item.
@@ -83,8 +83,45 @@ console.log(frutas)
 
 //Removendo um item do array com o método splice(indice do item, qtd de vezes que o indice será removido !)
 //Obs: Utilize o método indexOf(nomeItem) para descobrir o indice do item.
-let indice = frutas.indexOf("amora");
-console.log(`Elemento no indice antes da remoção: ${frutas[indice]}`);
-frutas.splice(indice, 1);
-console.log(frutas);
-console.log(`Elemento no indice após a remoção: ${frutas[indice]}`);
+// let indice = frutas.indexOf("amora");
+// console.log(`Elemento no indice antes da remoção: ${frutas[indice]}`);
+// frutas.splice(indice, 1);
+// console.log(frutas);
+// console.log(`Elemento no indice após a remoção: ${frutas[indice]}`);
+
+// //Impressao do array em tabela
+// console.table(frutas);
+
+// frutas.forEach(fruta=>{
+//     console.log("FRUTA DA VEZ : " + fruta);
+// } );
+
+// let nr1 = [1,2,3,4,5];
+// let nr2 = [6,7,8,9,10];
+// console.log("ARRAY 1 : " + nr1);
+// console.log("ARRAY 2 : " + nr2);
+// //Concatenando arrays em um novo array.
+// let nr3 = [nr1,nr2];
+// console.log("NOVO ARRAY : " + nr3);
+// //Imprimindo o novo array com forEach
+// nr3.forEach((nr)=>{
+//     nr.forEach((n)=>{
+//         console.log("ITENS DO NOVO ARRAY : " + n);
+//     });
+//     //console.log("ITENS DO NOVO ARRAY : " + nr);
+// });
+
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+console.log("ARRAY 1 : " + nr1);
+console.log("ARRAY 2 : " + nr2);
+//Concatenando arrays em um novo array.= com operador SPREAD(...);
+let nr3 = [...nr1,...nr2];
+console.log("NOVO ARRAY : " + nr3);
+//Imprimindo o novo array com forEach
+nr3.forEach((nr)=>{
+    //nr.forEach((n)=>{
+        console.log("ITENS DO NOVO ARRAY : " + nr);
+    //});
+    
+});
